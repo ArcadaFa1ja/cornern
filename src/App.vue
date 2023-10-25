1f0cde44cb0a60538ff88c420c2cc28d8bb30da0
Saile336 <elias.nybondas@gmail.com>
2023-10-26 01:15:43 +0300

    vue setup clock setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <script>
 
+import TheClock from "./components/TheClock.vue";
+
 export default {
-  
+  components: {
+    TheClock
+  },
 }
 
 </script>
 
 <template>
+  <img id="logo" src="src/assets/images/tlklogo-white.png" alt="tlklogo">
 
-    <img id="logo" src="src/assets/images/tlklogo-white.png" alt="tlklogo">
-
+  <div id="theClock">
+    <TheClock />
+  </div>
 </template>
 
 <style scoped>
 
+
 #logo {
-    max-width: 100%;
-    height: auto;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    height: 500px;
-    width: 500px;
-    opacity: 0.3; /* kan vara full opacity as well */
+  max-width: 100%;
+  height: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 500px;
+  width: 500px;
+  opacity: 0.3;
+  /* kan vara full opacity as well */
 }
+
+#theClock{
+  position: absolute;
+  top: 5vh;
+  left: 2vw;
+  height: fit-content;
+  width: fit-content;
+}
+
 </style>
